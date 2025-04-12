165fe27a46ee4600092cb6406b8898040bb1cd16
KliinM <calin.marinescu2209@gmail.com>
2025-04-12 21:27:41 +0300

    Add files via upload
    Scuze de intarziere,joi am avut doua teste si a trebuia sa invat miercuri seara,si joia seara am avut de invatat pentru un test la mate,am facut.Ultima cerinta adica diferenta poziitilor divizibile cu 3 nu sunt sigur daca este corect si daca am inteles o.Daca nu era cum am facut eu la pozitiile divizibile cu 3 te rog frumos sa mi spui ca sa facem cat de repede posibil adica luni

---
+++ Tema array.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int lungime = 0,suma = 0,sumap=0,produsI=1,i=0;
+ long poz=0;
+ int poz2;
+  cout << "Lungime = " ;
+  cin >> lungime;
+  int cars[lungime]; // = {"Volvo", "BMW", "Ford", "Mazda"};
+  for (int i = 0; i< lungime; i++)
+  {
+  cout << "cars[" << i << "] =";
+    cin >> cars[i];
+    suma+=cars[i];
+    if(i%2==0 || i==0)
+    {
+         sumap+=cars[i];
+    }
+            if(i%2==1 )
+            {
+                produsI*=cars[i];
+            }
+                    //cout << cars[i] <<"\n";
+  }
+
+for (int i = 0; i< lungime; i++)
+  {
+      if(i%3==0)
+      {
+          poz=cars[i]-poz;
+      }
+
+
+
+
+
+  }
+  cout << suma <<"\n";
+  cout<<sumap<<"\n";
+  cout<<produsI<<"\n";
+  cout<<poz;
+  return 0;
+}
