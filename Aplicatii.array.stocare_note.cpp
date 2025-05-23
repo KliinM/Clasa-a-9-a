969047d8145f33223b3d89a979ef1fe8ee7493be
KliinM <calin.marinescu2209@gmail.com>
2025-05-23 19:21:44 +0300

    Add files via upload

--- Aplicatii.array.stocare_note.cpp
+++ Aplicatii.array.stocare_note.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+
 using namespace std;
 
 int main() {
     const int SIZE = 10;
-    int int_p, int_n = 0, int_min, int_max;
+    int int_p, int_n = 0, int_min, int_max,nota_temp, j;
     string int_nume_copii[SIZE] = {"Maria", "Aby", "Laurentiu", "Mihaela", "Petrica", "Elena", "Olivia", "Eva", "Calin", "Luca"};
     int int_copii_note[SIZE];
-
+     string nume_temp;
     cout << "Introdu nr de copii de la 1 la 10: ";
     cin >> int_p;
     if (int_p > SIZE || int_p < 1) {
@@ -36,6 +37,21 @@ int main() {
     int parte_intreaga_media = int_n / int_p;
     int rest_media = int_n % int_p;
 
+    for (int i = 1; i < int_p; i++) {
+         nume_temp = int_nume_copii[i];
+         nota_temp = int_copii_note[i];
+        j = i-1;
+
+        while (j >= 0 && int_nume_copii[j] > nume_temp) {
+            int_nume_copii[j + 1] = int_nume_copii[j];
+            int_copii_note[j + 1] = int_copii_note[j];
+            j--;
+        }
+
+        int_nume_copii[j + 1] = nume_temp;
+        int_copii_note[j + 1] = nota_temp;
+    }
+
     cout << "Cel mai mare numar: " << int_max << "\n";
     cout << "Cel mai mic numar: " << int_min << "\n";
     cout << "Media aritmetica a elevilor este: " << parte_intreaga_media << "." << rest_media << "\n";
@@ -51,19 +67,17 @@ int main() {
 
     cout << setfill('-') << setw(25) << "-" << endl;
 
+    cout << "Elevii care indeplinesc conditia |nota - media| < 1\n";
+    int media_aproximativa = parte_intreaga_media;
 
-
-cout << "Elevii care indeplinesc conditia |nota - media| < 1\n";
-int media_aproximativa = parte_intreaga_media;
-
-if (rest_media * 2 > int_p) {
-    media_aproximativa = parte_intreaga_media + 1;
-}
-for (int i = 0; i < int_p; i++) {
-    if (int_copii_note[i] == parte_intreaga_media || int_copii_note[i] == parte_intreaga_media + 1) {
-        cout << int_nume_copii[i] << " - Nota: " << int_copii_note[i] << "\n";
+    if (rest_media * 2 > int_p) {
+        media_aproximativa = parte_intreaga_media + 1;
     }
-}
+    for (int i = 0; i < int_p; i++) {
+        if (int_copii_note[i] == parte_intreaga_media || int_copii_note[i] == parte_intreaga_media + 1) {
+            cout << int_nume_copii[i] << " - Nota: " << int_copii_note[i] << "\n";
+        }
+    }
 
     return 0;
 }
