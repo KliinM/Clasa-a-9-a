90b2ac1ecd7f4a6b48819734050068e54ed27f1e
Halford13 <55823550+Halford13@users.noreply.github.com>
2025-06-20 13:12:04 -0400

    Update Array.Informatii.cpp
    display_array

--- Array.Informatii.cpp
+++ Array.Informatii.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+void display_array (string arr_CapeteTabel[], string arr_Valori[][])
+{
+    /*
+    int lungimeTotala = nrColoane * LATIME_CELULA;
+    for (int i = 0; i < lungimeTotala; i++) cout << "-";
+    cout << endl;
 
+    for (int i = 0; i < nrColoane; i++) {
+        cout << setw(LATIME_CELULA) << left << capeteColoane[i];
+    }
+    cout << endl;
+
+    for (int i = 0; i < lungimeTotala; i++) cout << "-";
+    cout << endl;
+
+    for (int i = 0; i < nrRanduri; i++) {
+        for (int j = 0; j < nrColoane; j++) {
+            cout << setw(LATIME_CELULA) << left << tabel[i][j];
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < lungimeTotala; i++) cout << "-";
+    cout << endl;
+    */
+    
+}
 int main() {
     const int MAX_COLOANE = 10;
     const int MAX_RANDURI = 20;
     const int LATIME_CELULA = 15;
-
+//input
     string capeteColoane[MAX_COLOANE];
     string tabel[MAX_RANDURI][MAX_COLOANE];
 
@@ -31,28 +57,9 @@ int main() {
             cin >> tabel[i][j];
         }
     }
-
-    int lungimeTotala = nrColoane * LATIME_CELULA;
-    for (int i = 0; i < lungimeTotala; i++) cout << "-";
-    cout << endl;
-
-    for (int i = 0; i < nrColoane; i++) {
-        cout << setw(LATIME_CELULA) << left << capeteColoane[i];
-    }
-    cout << endl;
-
-    for (int i = 0; i < lungimeTotala; i++) cout << "-";
-    cout << endl;
-
-    for (int i = 0; i < nrRanduri; i++) {
-        for (int j = 0; j < nrColoane; j++) {
-            cout << setw(LATIME_CELULA) << left << tabel[i][j];
-        }
-        cout << endl;
-    }
-
-    for (int i = 0; i < lungimeTotala; i++) cout << "-";
-    cout << endl;
+    
+// display 
+    display_array(capeteColoane[j], tabel[i][j])
+    
 
     return 0;
-}
